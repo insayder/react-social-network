c4d65b2aadcde1184d481f12afcbfca4cbcee2f1
Dmitriy Dadorin <36156153+insayder@users.noreply.github.com>
2018-11-25 16:47:25 +0300

    add-task-list (#9)

--- src/containers/Tasks/Tasks.js
+++ src/containers/Tasks/Tasks.js
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import App from './components/app';
 import styles from './Tasks.module.css'
 
 function Tasks(props) {
   return (
     <div className={styles.tasks}>
-      <h1>Tasks</h1>
+      <App />
     </div>
   )
 }
 
 export default Tasks
+
